e3113efa986e0374f1cd09c6836e1c0511c03d08
AishMishra001 <aishlunatic001@gmail.com>
2025-05-09 19:34:33 +0530

    get number function

--- Question1/index.js
+++ Question1/index.js
@@ -3,7 +3,28 @@ const axios = require("axios");
 
 const app = express();
 const PORT = 9876;
+const WINDOW_SIZE = 10;
+let windowStore = [];
 
+const validIds = ['p', 'f', 'e', 'r'];
+
+const getNumbersFromTestServer = async (type) => {
+  const controller = new AbortController();
+  const timeout = setTimeout(() => controller.abort(), 500);
+
+  try {
+    const response = await axios.get(`http://20.244.56.144/numbers/${type}`, {
+      signal: controller.signal,
+      timeout: 500,
+    });
+    clearTimeout(timeout);
+    return response.data.numbers || [];
+  } catch (error) {
+    clearTimeout(timeout);
+    console.log("Error or timeout fetching:", type);
+    return [];
+  }
+};
 
 
 app.listen(PORT, () => {
